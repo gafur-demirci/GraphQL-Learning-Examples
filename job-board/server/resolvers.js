8a3985a80813c2fca748cca8f60772850ff36583
gafur-demirci <gafur.gs.68@gmail.com>
2022-12-08 13:27:42 +0300

    Updated schema and resolvers for job with company feature

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,5 +1,16 @@
+import { Company, Job } from './db.js';
+
 export const resolvers = {
-    Query : {
-        jobs: () => [],
+    Query: {
+        jobs: () => Job.findAll(),
+    },
+    Job: {
+        company: (job) => { Company.findById(job.companyId);
+            // console.log('resolving company for Job', job)
+            // return {
+            //     id: 'Fake',
+            //     name: 'Fake Inc.'
+            // };
+        },
     },
 };
