45e2515f79e2e529f767df0ac58260ed7ed9937e
gafur-demirci <gafur.gs.68@gmail.com>
2022-12-08 14:47:19 +0300

    Updated for reached to jobs on client to ApolloServer data

--- job-board/client/src/components/JobBoard.js
+++ job-board/client/src/components/JobBoard.js
@@ -1,7 +1,15 @@
 import JobList from './JobList';
-import { jobs } from '../fake-data';
+import { getJobs } from '../graphql/queries';
+import { useEffect, useState } from 'react';
+
 
 function JobBoard() {
+  const [jobs, setJobs] = useState([]);
+  useEffect( () => {
+    getJobs().then(setJobs)
+  }, [])
+
+  console.log('[JobBoards] Jobs:' , jobs);
   return (
     <div>
       <h1 className="title">
