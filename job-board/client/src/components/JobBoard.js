38564ede47a8f61df8b7e2e7f9d8a74bfdef1179
gafur-demirci <gafur.gs.68@gmail.com>
2022-12-08 17:36:43 +0300

    Updated for errorHandling

--- job-board/client/src/components/JobBoard.js
+++ job-board/client/src/components/JobBoard.js
@@ -4,20 +4,25 @@ import { useEffect, useState } from 'react';
 
 
 function JobBoard() {
-  const [jobs, setJobs] = useState([]);
-  useEffect( () => {
-    getJobs().then(setJobs)
-  }, [])
+	const [jobs, setJobs] = useState([]);
+	const [error, setError] = userState(false);
+	useEffect(() => {
+		getJobs().then(setJobs)
+			.catch((error) => setError(true))
+	}, [])
 
-  console.log('[JobBoards] Jobs:' , jobs);
-  return (
-    <div>
-      <h1 className="title">
-        Job Board
-      </h1>
-      <JobList jobs={jobs} />
-    </div>
-  );
+	console.log('[JobBoards] Jobs:', jobs);
+	if (error) {
+		return <p>Sorry, something went wrong.</p>;
+	}
+	return (
+		<div>
+			<h1 className="title">
+				Job Board
+			</h1>
+			<JobList jobs={jobs} />
+		</div>
+	);
 }
 
 export default JobBoard;
