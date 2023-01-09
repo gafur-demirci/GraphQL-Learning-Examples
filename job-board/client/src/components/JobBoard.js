5a87d8eee3f0620d938cdce18870625d4dbabb2b
gafur-demirci <gafur.gs.68@gmail.com>
2023-01-09 20:59:43 +0300

    JobBoard modified

--- job-board/client/src/components/JobBoard.js
+++ job-board/client/src/components/JobBoard.js
@@ -1,20 +1,28 @@
 import JobList from './JobList';
-import { getJobs } from '../graphql/queries';
-import { useEffect, useState } from 'react';
+import { JOBS_Query } from '../graphql/queries';
+// import { useEffect, useState } from 'react';
+import { useQuery } from '@apollo/client';
 
 
 function JobBoard() {
-	const [jobs, setJobs] = useState([]);
-	const [error, setError] = useState(false);
-	useEffect(() => {
-		getJobs().then(setJobs)
-			.catch((error) => setError(true))
-	}, [])
-
-	console.log('[JobBoards] Jobs:', jobs);
+	const {data, loading, error} = useQuery(JOBS_Query, {
+		fetchPolicy: 'network-only'
+	});
+	// const [jobs, setJobs] = useState([]);
+	// const [error, setError] = useState(false);
+	// useEffect(() => {
+	// 	getJobs().then(setJobs)
+	// 		.catch((error) => setError(true))
+	// }, [])
+ 
+	console.log('[JobBoards] Jobs:', {data, loading, error});
+	if(loading) {
+		return <p>Loading...</p>
+	}
 	if (error) {
 		return <p>Sorry, something went wrong.</p>;
 	}
+	const { jobs } = data;
 	return (
 		<div>
 			<h1 className="title">
