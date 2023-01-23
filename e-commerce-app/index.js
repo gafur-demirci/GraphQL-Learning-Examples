cd8f15c6f64a00c83c10fa0fd09761b6b899390e
gafur-demirci <gafur.gs.68@gmail.com>
2023-01-23 16:52:41 +0300

    Basic Apollo Server Configured

--- e-commerce-app/index.js
+++ e-commerce-app/index.js
@@ -1,13 +1,32 @@
-const { ApolloServer, gql} = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server');
+
+// Scaler Types : String, Int, Float, Boolean
 
 const typeDefs = gql`
 
     type Query {
         hello : String
+        age: Int
+        salary: Float
+        isLive: Boolean
     }
 
 `;
 
 const resolvers = {
+    Query: {
+        hello: () => { return "Hello World!" },
+        age: () => { return 28 },
+        salary: () => { return 123456.123 },
+        isLive: () => { return false },
+    },
+};
 
-}
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(( { url} ) => {
+    console.log(`Server running on ${url} a live`);
+})
