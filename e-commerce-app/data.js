d52c651a040f6bd155ff2fb37b177f69103631c5
gafur-demirci <gafur.gs.68@gmail.com>
2023-02-02 09:42:08 +0300

    Graphql data move to data.js

---
+++ e-commerce-app/data.js
@@ -0,0 +1,43 @@
+// Scaler Types : String, Int, Float, Boolean
+
+exports.products = [
+    {
+        id: "1",
+        name: "Black Shoe",
+        description: "New Black Shoes",
+        quantity: 5,
+        price: 100.50,
+        onSale: true
+    },
+    {
+        id: "2",
+        name: "White Shoe",
+        description: "New White Shoes",
+        quantity: 6,
+        price: 110.50,
+        onSale: false
+    },
+    {
+        id: "3",
+        name: "Red Shoe",
+        description: "New Red Shoes",
+        quantity: 7,
+        price: 120.50,
+        onSale: true
+    }
+];
+
+exports.categories = [
+    {
+        id: "1",
+        name: "Men"
+    },
+    {
+        id: "2",
+        name: "Women"
+    },
+    {
+        id: "3",
+        name: "Child"
+    }
+]
--- e-commerce-app/index.js
+++ e-commerce-app/index.js
@@ -1,47 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server');
-
-// Scaler Types : String, Int, Float, Boolean
-const products = [
-    {
-        id: "1",
-        name: "Black Shoe",
-        description: "New Black Shoes",
-        quantity: 5,
-        price: 100.50,
-        onSale: true
-    },
-    {
-        id: "2",
-        name: "White Shoe",
-        description: "New White Shoes",
-        quantity: 6,
-        price: 110.50,
-        onSale: false
-    },
-    {
-        id: "3",
-        name: "Red Shoe",
-        description: "New Red Shoes",
-        quantity: 7,
-        price: 120.50,
-        onSale: true
-    }
-];
-
-const categories = [
-    {
-        id: "1",
-        name: "Men"
-    },
-    {
-        id: "2",
-        name: "Women"
-    },
-    {
-        id: "3",
-        name: "Child"
-    }
-]
+const { products, categories } = require('./data.js');
 
 const typeDefs = gql`
 
