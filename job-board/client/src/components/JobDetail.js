e3f7328cbbac4376234825d7315f261f1aad638e
gafur-demirci <gafur.gs.68@gmail.com>
2023-04-21 14:07:23 +0300

    removed commentted lines

--- job-board/client/src/components/JobDetail.js
+++ job-board/client/src/components/JobDetail.js
@@ -1,17 +1,10 @@
-// import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import { useJob } from '../graphql/hooks';
-// import { getJobById } from '../graphql/queries';
-
 
 function JobDetail() {
-  // const [job, setJob] = useState(null);
   const { jobId } = useParams();
   const { job, loading } = useJob(jobId);
-  // useEffect(() => {
-  //   getJobById(jobId).then(setJob);
-  // }, [jobId]);
 
   console.log('[JobDeatil] job: ' ,job);
   if(loading){
